d034148a5c5eadf2c6f35ef68be14d0c4eef74b7
Sai-23 <saibhot23@gmail.com>
2023-03-27 15:15:58 +0530

    validation in add student

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         '/mainPage': (context) => const mainPage(),
         '/addStudent': (context) => const addStudent(),
         '/displayStudent':(context)=>const DisplayStudent()
-      },
+      }
     );
   }
 }
